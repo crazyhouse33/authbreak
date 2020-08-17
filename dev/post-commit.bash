7b8b00951e312865cf04c23c1dd3dbc7a2eb3230
crazyhouse33 <hugo.alonso@etu.u-bordeaux.fr>
2020-08-18 01:07:39 +0200

    Added automatic merge with perf at end of commit

--- dev/post-commit.bash
+++ dev/post-commit.bash
@@ -1,4 +1,14 @@
 set -e
-git commit -a --amend -C
-git checkout perf
-git merge dev
+unset GIT_DIR
+if [[ -z "${SKIP_POST_COMMIT}" ]]; then
+	echo going to perf branch 
+	git checkout perf
+
+	echo merge dev
+	git merge dev
+else
+	echo including pre-commit change
+	SKIP_POST_COMMIT=true git commit -a --amend --no-verify --no-edit
+	
+fi
+
